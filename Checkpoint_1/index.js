cc1e69a1efd664bdb09289110fb47c08c6b54997
Lucas5497 <lucasanselmoluizlopes@gmail.com>
2021-11-10 23:01:21 -0300

    Update index.js

--- Checkpoint_1/index.js
+++ Checkpoint_1/index.js
@@ -3,19 +3,17 @@ conforme solicitado. Veja abaixo os requisitos do projeto:
 
 - Precisamos desenvolver um menu para um microondas super veloz, onde teremos 5 opções de comida com seus respectivos tempos 
 pré-definidos. 
-
-      1 - Pipoca – 10 segundos (padrão);
-      2 - Macarrão – 8 segundos (padrão);
-      3 - Carne – 15 segundos (padrão);
-      4 - Feijão – 12 segundos (padrão);
-      5 - Brigadeiro – 8 segundos (padrão); 
-
-- O usuário poderá alterar o tempo padrão, aumentando ou diminuindo de acordo com sua vontade. Se o tempo informado for maior
+      1 - Pipoca - 10 segundos (padrão);
+      2 - Macarrão - 8 segundos (padrão);
+      3 - Carne - 15 segundos (padrão);
+      4 - Feijão - 12 segundos (padrão);
+      5 - Brigadeiro - 8 segundos (padrão); 
+- O usuário pode alterar o tempo padrão, aumento ou diminuindo de acordo com sua vontade. Se o tempo técnico para maior
  que 2x o necessário, exibir mensagem que a comida queimou.
-- Se o tempo for menor que o padrão, exibir a mensagem: "tempo insuficiente"; 
-- Opções não listadas no menu, devem exibir uma mensagem de erro: "Prato inexistente";
+- Se o tempo for menor que o padrão, exibir uma mensagem: "tempo insuficiente"; 
+- Opções não listadas no menu, exibir uma mensagem de erro: "Prato inexistente";
 - Se o tempo for 3x maior que o necessário para o prato, o microondas deve exibir a mensagem: “kabumm”;
-- No final de cada tarefa, o microondas deverá exibir a mensagem: "Prato pronto, bom apetite!!!".*/
+- No final de cada tarefa, o microondas deve exibir a mensagem: "Prato pronto, bom apetite !!!". */
 
 let pipoca = 10;
 let macarrão = 8;
@@ -23,16 +21,23 @@ let carne = 15;
 let feijão = 12;
 let brigadeiro = 8;
 
-function esquentar(comida, tempo){
-    
-    if (tempo >= comida * 2 && tempo < comida * 3 ){
-        console.log("A comida queimou")
-    } else if(tempo >= comida * 3 ){
-        console.log("Kabumm")
-    } else if(tempo < comida){
-        console.log("Tempo insuficiente")
-    } else console.log("Prato pronto, bom apetite!!!")
+function esquentar(comida, tempo) {
+      
+      if (tempo > comida && tempo <= comida * 2){
+            console.log("Prato pronto, bom apetite!!!")
+      } else if (tempo >= comida * 2 && tempo < comida * 3) {
+            console.log("A comida queimou\nPrato pronto, bom apetite!!!")
+            
+      } else if (tempo >= comida * 3) {
+            console.log("Kabumm\nPrato pronto, bom apetite!!!")
+            
+      } else if (tempo < comida) {
+            console.log("Tempo insuficiente\nPrato pronto, bom apetite!!!")
+      } 
 
 }
 
-esquentar(macarrão, 15);
+esquentar(pipoca, 21)
+
+
+
